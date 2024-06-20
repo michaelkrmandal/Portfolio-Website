2516e1856078d8014070f664ff6f5e175c5d0861
Michael Kumar Mandal <127439951+michaelkrmandal@users.noreply.github.com>
2024-06-20 19:40:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /* ======typed js====== */
 const typed = new Typed('.multiple-text', {
-  strings: ['Frontend Developer', 'Software Engineer', 'AI Tools and ChartGpt Learner'],
+  strings: ['Web Developer', 'Software Engineer', 'AI Tools Learner'],
   typeSpeed: 100,
   backSpeed: 100,
   backdelay: 1000,
